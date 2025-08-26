b452d80ac1955cb15317d9df75a9a66db8744e30
Boo-coder <Bouna112@yahoo.com>
2025-08-25 22:38:57 -0400

    Objt Ort

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your code here
+class Breakfast{
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+    } 
+}
+
+let brkFast = new Breakfast("eggs", "juice");
+brkFast;
+
+
+class Lunch{
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+let lch = new Lunch("side salad", "broccoli cheddar soup", "iced tea");
+lch;
+
+
+class Dinner{
+    #dessert;
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+}
+
+let dnr = new Dinner("balsamic salad", "consomme", "filet mignon", "cheesecake");
+dnr;
